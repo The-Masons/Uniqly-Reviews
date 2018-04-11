07ec349d478b49b6a21348055869c4aba8e99345
lhmun <louis.mun@gmail.com>
2018-04-11 16:41:13 -0700

    Getting data from mongodb

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
+const Reviews = require('../database/Review.js');
+
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/reviews', (req, res) => {
+    Reviews.find({nickname: 'Louis'}, (err, results)=>{
+        console.log('========', results);
+        res.send(results);
+    });
+
+});
 
 app.listen(3005, () => console.log('Server listening on port 3005!'))
