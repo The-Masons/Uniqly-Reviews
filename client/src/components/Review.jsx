e51329845db5a002fc6323e3db3779219fe26f9e
lhmun <louis.mun@gmail.com>
2018-04-21 14:15:03 -0700

    Renamed Body to ReviewBody for consistency; Changed class to stateless component

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import UserInfo from './UserInfo.jsx';
-import Body from './Body.jsx';
+import ReviewBody from './ReviewBody.jsx';
 import Ratings from './Ratings.jsx';
 
 let Review = (props) => {
     return (
         <div className='review-component'>
-            <UserInfo reviewData={props.reviewData} /><Body reviewData={props.reviewData} /><Ratings reviewData={props.reviewData} />
+            <UserInfo reviewData={props.reviewData} /><ReviewBody reviewData={props.reviewData} /><Ratings reviewData={props.reviewData} />
         </div>
     )
 }
