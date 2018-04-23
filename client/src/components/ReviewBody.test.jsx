e2c87e285a35fca32e0fd68c4aa0440dbfd3c4a8
lhmun <louis.mun@gmail.com>
2018-04-23 11:51:09 -0700

    Setup basic tests

--- client/src/components/ReviewBody.test.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReviewBody from './ReviewBody.jsx';
-
-it('renders', () => {
-  const wrapper = shallow(<ReviewBody />);
-  expect(wrapper.find('p').text()).toEqual('testing');
-});
